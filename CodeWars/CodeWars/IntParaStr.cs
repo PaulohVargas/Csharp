fcc50477c2c1a4aa54004ab3b08595886bac8e23
PaulohVargas <popenkoski@gmail.com>
2022-07-03 22:41:32 -0300

    Iniciando CodeWars

---
+++ CodeWars/CodeWars/IntParaStr.cs
@@ -0,0 +1,6 @@
+﻿using System;
+public class Kata {
+    public static int StringToNumber(String str) {
+        return int.Parse(str);
+    }
+}
---
+++ CodeWars/CodeWars/NumeroInverso.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Kata {
+    public static int Opposite(int number) {
+        number = number * -1;
+        return number;
+    }
+}
